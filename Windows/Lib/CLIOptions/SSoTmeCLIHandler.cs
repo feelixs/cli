db8a913611aaedbbcce71fbf7ab1fe3f53268c81
eejai42 <ej@ssot.me>
2023-02-25 02:59:35 -0600

    Moved AICManager code into it's own class/file.

--- Windows/Lib/CLIOptions/SSoTmeCLIHandler.cs
+++ Windows/Lib/CLIOptions/SSoTmeCLIHandler.cs
@@ -252,24 +252,8 @@ namespace SSoTme.OST.Lib.CLIOptions
                 }
                 else if (this.discuss)
                 {
-                    var aica = new AIC.SassyMQ.Lib.SMQAICAgent("amqps://smqPublic:smqPublic@effortlessapi-rmq.ssot.me/ej-aicapture-io");
-                    aica.UserAICInstallReceived += Aica_UserAICInstallReceived;
-                    aica.UserAICReplayReceived += Aica_UserAICReplayReceived;
-                    aica.UserSetDataReceived += Aica_UserSetDataReceived;
-                    aica.UserGetDataReceived += Aica_UserGetDataReceived;
-                    var payload = aica.CreatePayload();
-                    payload.AccessToken = this.Auth0SID;
-                    payload.DMQueue = aica.QueueName;
-                    var reply = aica.MonitoringFor(payload);
-
-
-                    Console.WriteLine($"Listening on DMQueue: {aica.QueueName}. Press Ctrl+C to end.");
-                    while (!Console.KeyAvailable)
-                    {
-                        aica.WaitForComplete(1000, false);
-                    }
-                    Console.ReadKey();
-                    aica.Disconnect();
+                    var aicManager = AICManager.Create(this.Auth0SID);
+                    aicManager.Start();
                 }
                 else if (this.checkResults || this.createDocs && !hasRemainingArguments)
                 {
@@ -346,44 +330,6 @@ namespace SSoTme.OST.Lib.CLIOptions
             }
         }
 
-        private void Aica_UserAICInstallReceived(object sender, AIC.SassyMQ.Lib.PayloadEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Aica_UserGetDataReceived(object sender, AIC.SassyMQ.Lib.PayloadEventArgs e)
-        {
-            e.Payload.AICaptureProjectFolder = $"/{Path.GetFileName(Environment.CurrentDirectory)}";
-            var found = this.LookFor("single-source-of-truth.json", e.Payload);
-            if (!found) found = this.LookFor("ssot.json", e.Payload);
-            if (!found) found = this.LookFor("aicapture.json", e.Payload);
-        }
-
-        private bool LookFor(string fileName, AIC.SassyMQ.Lib.StandardPayload payload)
-        {
-            var fi = new FileInfo(fileName);
-            if (fi.Exists) return FoundFile(payload, fi);
-            fi = new FileInfo(Path.Combine("ssot", fileName));
-            if (fi.Exists) return FoundFile(payload, fi);
-            return false;
-        }
-
-        private bool FoundFile(AIC.SassyMQ.Lib.StandardPayload payload, FileInfo fi)
-        {
-            payload.FileName = fi.FullName.Substring(Environment.CurrentDirectory.Length);
-            payload.Content = File.ReadAllText(fi.FullName);
-            return true;
-        }
-
-        private void Aica_UserSetDataReceived(object sender, AIC.SassyMQ.Lib.PayloadEventArgs e)
-        {
-            e.Payload.Content = $"/{Path.GetFileName(Environment.CurrentDirectory)}";
-        }
-
-        private void Aica_UserAICReplayReceived(object sender, AIC.SassyMQ.Lib.PayloadEventArgs e)
-        {
-            throw new Exception("Not setup to replay yet...");
-        }
 
         private bool CheckAuthenticationNow()
         {
@@ -600,6 +546,7 @@ namespace SSoTme.OST.Lib.CLIOptions
                 return auth0TokenFI;
             }
         }
+
 
         public void LoadInputFiles()
         {
