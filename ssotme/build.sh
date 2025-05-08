bf37e5da90823d11208c1d22f3b3791d5193a701
Michael Felix <michael@felixcreations.com>
2025-05-08 10:45:31 -0400

    build the exe

---
+++ ssotme/build.sh
@@ -0,0 +1,13 @@
+# bin/bash
+
+cd /Users/michaelfelix/Documents/GitHub/cli/
+
+# substitute setuptools with pyinstaller_setuptools
+sed -i '' 's/setuptools/pyinstaller_setuptools/' /Users/michaelfelix/Documents/GitHub/cli/setup.py
+
+source ~/.venv-3.12/bin/activate
+# run the build executable command
+python ./setup.py pyinstaller -- -n ssotme --console --onefile --add-data "ssotme:ssotme" --collect-all ssotme --hidden-import json
+
+# revert the setup.py change
+sed -i '' 's/pyinstaller_setuptools/setuptools/' /Users/michaelfelix/Documents/GitHub/cli/setup.py
