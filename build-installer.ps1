b068f3537a23b0a8ebcf7b31565f2e36c6d6301d
Michael Felix <michael@felixcreations.com>
2025-05-12 12:49:51 -0400

    mv to Installer/

--- build-installer.ps1
+++ build-installer.ps1
@@ -1,2 +1,2 @@
 # run the build script for creating the Windows msi installer
-powershell -ExecutionPolicy Bypass -File Windows/Installer/SSoTmeInstaller/Scripts/build.ps1
+powershell -ExecutionPolicy Bypass -File Windows/Installer/Scripts/build.ps1
