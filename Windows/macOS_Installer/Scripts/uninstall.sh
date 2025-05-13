0cbda6f553c1cbe40426f86237d74651cbc5a9fd
Michael Felix <michael@felixcreations.com>
2025-05-13 12:28:12 -0400

    simplify uninstall

--- Windows/macOS_Installer/Scripts/uninstall.sh
+++ Windows/macOS_Installer/Scripts/uninstall.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 # Uninstaller script for SSoTme CLI macOS package
-
-# Check if keep config parameter was passed
-KEEP_CONFIG="n"
-if [ $# -eq 1 ]; then
-    KEEP_CONFIG=$1
-fi
-
 echo "Uninstalling SSoTme CLI..."
 
 # Remove symbolic links from /usr/local/bin
@@ -17,17 +10,9 @@ sudo rm -f /usr/local/bin/aicapture
 # Remove application files
 sudo rm -rf /Applications/SSoTme
 
-# Check whether to remove config files
-if [[ ! $KEEP_CONFIG =~ ^[Yy]$ ]]; then
-    echo "Removing configuration files..."
-    rm -rf ~/.ssotme
-else
-    echo "Keeping configuration files in ~/.ssotme"
-fi
+echo "Removing configuration files..."
+rm -rf ~/.ssotme
 
 echo "SSoTme CLI has been uninstalled successfully."
-
-# Also remove the manager app itself
-sudo rm -rf "/Applications/SSoTme CLI Manager.app"
 
 exit 0
